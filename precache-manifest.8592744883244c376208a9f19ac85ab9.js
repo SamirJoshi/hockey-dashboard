cf8419e519aa3d7fd2596e10975f0a7ebf8e6d06
Samir Joshi <samir96joshi@gmail.com>
2019-04-09 20:06:48 -0700

    Updates

---
+++ precache-manifest.8592744883244c376208a9f19ac85ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73e1ba1a0f2af05635e1",
+    "url": "/hockey-dashboard/static/js/runtime~main.675b093b.js"
+  },
+  {
+    "revision": "444eb22503da2389be7b",
+    "url": "/hockey-dashboard/static/js/main.c069cc8a.chunk.js"
+  },
+  {
+    "revision": "c8bbbd2054e7e32caa7a",
+    "url": "/hockey-dashboard/static/js/2.7b2d24cf.chunk.js"
+  },
+  {
+    "revision": "444eb22503da2389be7b",
+    "url": "/hockey-dashboard/static/css/main.2c267170.chunk.css"
+  },
+  {
+    "revision": "c8bbbd2054e7e32caa7a",
+    "url": "/hockey-dashboard/static/css/2.8e68db52.chunk.css"
+  },
+  {
+    "revision": "b1b139e5bbd567c168db6bd500695b99",
+    "url": "/hockey-dashboard/index.html"
+  }
+];
