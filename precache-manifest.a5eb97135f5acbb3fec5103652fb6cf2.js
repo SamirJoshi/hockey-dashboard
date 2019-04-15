3d359c98db2d117fd54600d2707c9abe64f757cd
Samir Joshi <samir96joshi@gmail.com>
2019-04-14 22:57:50 -0700

    Updates

--- precache-manifest.a5eb97135f5acbb3fec5103652fb6cf2.js
+++ precache-manifest.a5eb97135f5acbb3fec5103652fb6cf2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/hockey-dashboard/static/js/runtime~main.675b093b.js"
   },
   {
-    "revision": "e85a755c673fac50c2e8",
-    "url": "/hockey-dashboard/static/js/main.8ca772fd.chunk.js"
+    "revision": "eba402348b911d4353da",
+    "url": "/hockey-dashboard/static/js/main.1fe6bcf7.chunk.js"
   },
   {
     "revision": "3ad071dd6e5b56e27eb1",
     "url": "/hockey-dashboard/static/js/2.6d15e389.chunk.js"
   },
   {
-    "revision": "e85a755c673fac50c2e8",
+    "revision": "eba402348b911d4353da",
     "url": "/hockey-dashboard/static/css/main.7310d71e.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/hockey-dashboard/static/css/2.cd5819bf.chunk.css"
   },
   {
-    "revision": "93db1bafa562557e3af6504ba67a8159",
+    "revision": "a41446bcd8db532b74eedfbc0afe2aad",
     "url": "/hockey-dashboard/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hockey-dashboard/precache-manifest.77f15676e62cce5ed04bc2daa54ddc78.js"
+  "/hockey-dashboard/precache-manifest.a5eb97135f5acbb3fec5103652fb6cf2.js"
 );
 
 workbox.clientsClaim();
