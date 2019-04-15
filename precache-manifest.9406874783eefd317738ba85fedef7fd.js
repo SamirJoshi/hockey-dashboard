20aaf7e8c5ca2ab77a1c19509f3abf08802cebce
Samir Joshi <samir96joshi@gmail.com>
2019-04-14 22:43:43 -0700

    Updates

---
+++ precache-manifest.9406874783eefd317738ba85fedef7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4887ea7418890e2fe04c",
+    "url": "https:/hockey-dashboard/static/js/runtime~main.768be4ae.js"
+  },
+  {
+    "revision": "9fd2d470e6964519fa09",
+    "url": "https:/hockey-dashboard/static/js/main.e6fce98a.chunk.js"
+  },
+  {
+    "revision": "d94bc079c152f97a369d",
+    "url": "https:/hockey-dashboard/static/js/2.cff7f478.chunk.js"
+  },
+  {
+    "revision": "9fd2d470e6964519fa09",
+    "url": "https:/hockey-dashboard/static/css/main.7310d71e.chunk.css"
+  },
+  {
+    "revision": "d94bc079c152f97a369d",
+    "url": "https:/hockey-dashboard/static/css/2.cd5819bf.chunk.css"
+  },
+  {
+    "revision": "8cd51043cbcd4ddab7f69ea4f22c1a04",
+    "url": "https:/hockey-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://samirjoshi.github.io/hockey-dashboard/precache-manifest.c8edd2a38f574adfed60a69b20b548f1.js"
+  "https:/hockey-dashboard/precache-manifest.9406874783eefd317738ba85fedef7fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://samirjoshi.github.io/hockey-dashboard/index.html", {
+workbox.routing.registerNavigationRoute("https:/hockey-dashboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
