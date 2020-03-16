4e324c9b085fe109cdd8298d9e535521b44a1bee
Samir Joshi <samir96joshi@gmail.com>
2020-03-15 20:09:41 -0700

    Serve built client from server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
+import * as createError from "http-errors"
 import * as express from "express"
 import * as cors from "cors"
+import * as path from "path"
 
 import rankingRouter from "./routes/rankingRouter"
 import comparisonRouter from "./routes/comparisonRouter"
@@ -8,25 +10,29 @@ const app = express()
 
 app.use(cors())
 
+// Serve the static files from the React app
+app.use(express.static('../client/build'))
+
 app.use('/comparison', comparisonRouter)
 app.use('/team', rankingRouter)
 app.use('/rankings', rankingRouter)
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) =>{
+  res.sendFile('../client/build/index.html')
+})
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+// error handler
+app.use((err: createError.HttpError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500).send()
+})
 
 const port = 3001
 app.listen(port, () => console.log(`Started listening on port ${port}`))
